da96a1410981dc0adbfbecd5247635af8fbb27c6
martinekmichal <martinek.m.84@gmail.com>
2024-08-29 18:52:22 +0200

    soucet + html plug <br>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import random
+from random import randint
+
 
 app = Flask(__name__)
 
@@ -18,6 +20,31 @@ def about():
 def cisla():
     nahodna_cisla = random.sample(range(1, 101), 3)
     return nahodna_cisla
+
+@app.route("/random/<count>")
+def random_page(count):
+    output = "Loteria: "
+    for i in range(int(count)):
+        output += f" {randint(1, 100)}, "
+
+    return output
+
+
+@app.route("/random2/<int:count>")
+def random_page2(count):
+    output = "Loteria: "
+    for i in range(count):
+        output = f" {randint(1, 100)}, "
+
+    return output
+
+@app.route("/soucet/<int:count>")
+def soucet(count):
+    nahodna_cisla = [random.randint(1, 101) for _ in range(count)]
+    soucet_cisel = sum(nahodna_cisla)
+    output = "Čísla: " + ", ".join(str(cislo) for cislo in nahodna_cisla)
+    output += (f" <br> Celkový součet: {soucet_cisel}")
+    return output
 
 if __name__ == "__main__":
     app.run(debug=False)

6e650e4b0cd2240bebecbd5bfe03d76889736a1e
martinekmichal <martinek.m.84@gmail.com>
2024-08-29 18:17:39 +0200

    cisla flask

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Nazdar světe"
+
+
+
+@app.route("/about")
+def about():
+    return "Okolo sekce "
+
+@app.route("/cisla")
+def cisla():
+    nahodna_cisla = random.sample(range(1, 101), 3)
+    return nahodna_cisla
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
